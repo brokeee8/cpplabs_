f4cdfbbe5063360c944034a3395f52086a96c96d
brokeee8 <neopunch@bk.ru>
2024-10-26 15:34:48 +0300

    lab4_part1

---
+++ lab4/lab4_home/lab4_home/lab4_home.cpp
@@ -0,0 +1,33 @@
+﻿// Лабораторная 4
+
+
+
+#include <iostream>
+using namespace std;
+
+class Weapon {
+public:
+    string name;
+    float damage;
+    float weight;
+
+    Weapon(string name, float damage, float weight) {
+        this->name = name;
+        this->damage = damage;
+        this->weight = weight;
+    }
+
+    Weapon() : Weapon("Weapon", 1, 1) {};
+};
+
+
+
+int main()
+{
+    Weapon pistol("Pistol", 10, 5);
+    Weapon ak = Weapon();
+
+    cout << pistol.name << " " << pistol.damage << " " << pistol.weight << endl;
+    cout << ak.name << " " << ak.damage << " " << ak.weight << endl;
+}
+
