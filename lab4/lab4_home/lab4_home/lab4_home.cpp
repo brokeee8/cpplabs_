d373e0670c77aede629f533e1ef76d825700728e
brokeee8 <neopunch@bk.ru>
2024-10-26 16:26:13 +0300

    lab4_part2

--- lab4/lab4_home/lab4_home/lab4_home.cpp
+++ lab4/lab4_home/lab4_home/lab4_home.cpp
@@ -1,8 +1,5 @@
-﻿// Лабораторная 4
-
-
-
-#include <iostream>
+﻿#include <iostream>
+#include <Windows.h>
 using namespace std;
 
 class Weapon {
@@ -11,23 +8,68 @@ public:
     float damage;
     float weight;
 
-    Weapon(string name, float damage, float weight) {
-        this->name = name;
-        this->damage = damage;
-        this->weight = weight;
-    }
+    Weapon(string name, float damage, float weight) : name(name), damage(damage), weight(weight) {}
 
     Weapon() : Weapon("Weapon", 1, 1) {};
+
+    ~Weapon() {
+        cout << "Объект " << name << " удаляется." << endl;
+    }
+
+    bool weightCheck(int maxWeight) const {
+        return weight > maxWeight;
+    }
+    
+    float weightSum(const Weapon& other) const{
+        return weightSum(other.weight);
+    }
+    float weightSum(float otherWeight) const {
+        return weight + otherWeight;
+    }
 };
+
 
 
 
 int main()
 {
-    Weapon pistol("Pistol", 10, 5);
-    Weapon ak = Weapon();
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    cout << pistol.name << " " << pistol.damage << " " << pistol.weight << endl;
-    cout << ak.name << " " << ak.damage << " " << ak.weight << endl;
+
+
+    //Weapon pistol("Pistol", 10, 5);
+    //Weapon w = Weapon();
+
+    //cout << pistol.name << " " << pistol.damage << " " << pistol.weight << endl;
+    //cout << w.name << " " << w.damage << " " << w.weight << endl;
+
+
+    Weapon shotgun("Shotgun", 25, 10);
+    cout << shotgun.name << " " << shotgun.damage << " " << shotgun.weight << endl;
+
+    Weapon* sword = new Weapon("Sword", 8, 6);
+    cout << sword->name << " " << sword->damage << " " << sword->weight << endl;
+
+    int maxWeight = 7;
+
+    if (shotgun.weightCheck(maxWeight)) {
+        cout << "Не сможет поднять оружие " << shotgun.name << endl;
+    }
+    else {
+        cout << "Сможет поднять оружие" << shotgun.name << endl;
+    }
+
+    if (sword->weightCheck(maxWeight)) {
+        cout << "Не сможет поднять оружие " << sword->name << endl;
+    }
+    else {
+        cout << "Сможет поднять оружие " << sword->name << endl;
+    }
+    
+    cout << "Суммарный вес Shotgun и Sword " << shotgun.weightSum(*sword) << endl;
+    cout << "Суммарный вес Shotgun и ... " << shotgun.weightSum(15) << endl;
+
+    delete sword;
 }
 
